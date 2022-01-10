fb90e55d9413c3c939224aac9004117cf7aee492
Ashen21853 <k.alvarez@mailoo.org>
2022-01-10 02:01:28 -0800

    add docker

--- setup_debian.sh
+++ setup_debian.sh
@@ -11,6 +11,14 @@
 #########################################################
 
 #####################
+###   DOCKER CE   ###
+#####################
+yes | apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#####################
 ###    COCKPIT    ###
 #####################
 COCKPIT_LISTENING_PORT=""               # Port to connect to the interface
